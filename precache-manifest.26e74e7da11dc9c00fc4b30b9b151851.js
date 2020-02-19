582f29535a50f60e22290aea77be00a5a5effc86
VIKRAM-PAREEK <vikrampareek920@gmail.com>
2020-02-19 13:59:42 +0530

    Updates

---
+++ precache-manifest.26e74e7da11dc9c00fc4b30b9b151851.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0cea0afb0ea12b7a0daaede7ed79918",
+    "url": "/Assignment2/index.html"
+  },
+  {
+    "revision": "7ddf4418af14d7030120",
+    "url": "/Assignment2/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "be021697b7cd92ee74d0",
+    "url": "/Assignment2/static/js/2.6315a0ef.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Assignment2/static/js/2.6315a0ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ddf4418af14d7030120",
+    "url": "/Assignment2/static/js/main.6cbae449.chunk.js"
+  },
+  {
+    "revision": "6d7941b07caa5cb3044e",
+    "url": "/Assignment2/static/js/runtime-main.e1066511.js"
+  }
+]);
