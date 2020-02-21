d32bdafe603a0e24041939e4aaf73fae3a158a2c
VIKRAM-PAREEK <vikrampareek920@gmail.com>
2020-02-21 18:53:47 +0530

    Starting with Material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import './App.css';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 
 class App extends React.Component{
   constructor(props){
@@ -31,25 +34,33 @@ class App extends React.Component{
   }
   render(){
     return(
-        
-        <div className="Todo Container">
-          <form id="todo-form"> 
-            <input 
-              value = {this.state.todoitem}
-              onChange={ (e)=>this.handleUserInput(e.target.value)}
-              type="text"
-              placeholder="To Do"
-              required
-            />
-            <button onClick={ (item)=>this.addToList(this.state.todoitem)} >
-              ADD
-            </button>
-            <ul>
-              {this.state.list.map( (val)=><li>{val}</li>)}
-            </ul>
-          </form>
-          
-        </div>
+
+            <Grid
+             container
+             direction="row"
+             justify="center"
+             alignItems="center"
+            >
+              <form id="todo-form"> 
+                <TextField id="outlined-basic" variant="outlined"
+                    value = {this.state.todoitem}
+                    onChange={ (e)=>this.handleUserInput(e.target.value)}
+                    type="text"
+                    placeholder="To Do"
+                    required 
+                />
+              </form>
+              <Button 
+                  variant="contained" 
+                  color="primary" 
+                  onClick={ (item)=>this.addToList(this.state.todoitem)} >
+                    ADD
+                </Button>
+                <ul>
+                  {this.state.list.map( (val)=><li>{val}</li>)}
+                </ul>
+
+            </Grid>        
       );
   }
 }
