9a10ba304155789a910cf745d45cdaf55718632b
VIKRAM-PAREEK <vikrampareek920@gmail.com>
2020-02-21 17:54:29 +0530

    Adding To Do items functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state={
+      todoitem: '',
+      list: []
+    }
+  }
+
+  //tapking the user input value
+  handleUserInput(item){
+    this.setState({
+      todoitem: item
+    });
+  }  
+
+  //functionality of button i.e adding todo items
+  addToList(item){
+    
+    let listArray = this.state.list; //creating a list and storing current todoitem values
+    listArray.push(item);
+
+    this.setState({
+      list: listArray, //storing the todo items in the main list
+      todoitem: ''    //Initialising the todoitem in the textbox as empty again
+    });
+  }
+  render(){
+    return(
+        
+        <div className="Todo Container">
+          <form id="todo-form"> 
+            <input 
+              value = {this.state.todoitem}
+              onChange={ (e)=>this.handleUserInput(e.target.value)}
+              type="text"
+              placeholder="To Do"
+              required
+            />
+            <button onClick={ (item)=>this.addToList(this.state.todoitem)} >
+              ADD
+            </button>
+            <ul>
+              {this.state.list.map( (val)=><li>{val}</li>)}
+            </ul>
+          </form>
+          
+        </div>
+      );
+  }
 }
 
 export default App;
